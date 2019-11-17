4eb31ee92e632ff8bbcbda8f3b512e33f642c3ae
Jairus Bali <jairus.bali@gmail.com>
2019-11-17 01:54:26 -0500

    added material-ui lib for slider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import React from "react";
+import Slider from "@material-ui/core/Slider";
 
 import Lottery from "./components/Lottery/Lottery";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Lottery title="Lottery Generator" min={1} max={7} numberOfBalls={2} />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: [0, 10]
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.valuetext = this.valuetext.bind(this);
+  }
+
+  handleChange(event, newValue) {
+    this.setState({ value: newValue });
+  }
+
+  valuetext(value) {
+    return `${value}°C`;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Lottery title="Lottery Generator" min={1} max={7} numberOfBalls={2} />
+        <Slider
+          value={this.state.value}
+          onChange={this.handleChange}
+          valueLabelDisplay="auto"
+          aria-labelledby="range-slider"
+          getAriaValueText={this.valuetext}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
